8e575871e5f196b0e04c9d1af72f47e9fcddf01f
La Shell Slaughter <lashellslaughter109@gmail.com>
2020-09-29 19:41:03 -0500

    Added code

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AreaOfACircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi;
+            pi = 3.14;
+            double radius;
+            double area;
+            double circumference;
+            double diameter;
+            double mpg;
+            double gallons;
+            string input;
+
+            Console.WriteLine("Enter a radius of a circle:");
+            input = Console.ReadLine();
+            radius = double.Parse(input);
+
+            area = pi * radius * radius;
+            circumference = 2 * pi * radius;
+            diameter = 2 * radius;
+
+            Console.WriteLine("The area of a circle of radius " + radius + " is: " + area);
+            Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + circumference);
+            Console.WriteLine("The diameter of a circle of radius " + radius + " is: " + diameter);
+
+            Console.WriteLine("Enter the miles per gallon of your car:");
+            input = Console.ReadLine();
+            mpg = double.Parse(input);
+            gallons = mpg / radius;
+
+            Console.WriteLine("If the radius you entered was in miles, you would use " + gallons + " gallons of gas around the circle.");
+            Console.ReadLine();
+        }
+    }
+}
